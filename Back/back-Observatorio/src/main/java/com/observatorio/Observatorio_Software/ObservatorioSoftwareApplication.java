601a3a8845bd7209cfe4bea4ae03bf7b56ac818e
Joan Sebastian Tuquerrez Gomez <jtuquerrez@unicauca.edu.co>
2024-09-25 18:43:05 -0500

    First aproach of the Back in Observatio Software

---
+++ Back/back-Observatorio/src/main/java/com/observatorio/Observatorio_Software/ObservatorioSoftwareApplication.java
@@ -0,0 +1,13 @@
+package com.observatorio.Observatorio_Software;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObservatorioSoftwareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObservatorioSoftwareApplication.class, args);
+	}
+
+}

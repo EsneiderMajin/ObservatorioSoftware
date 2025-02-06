5cadd21042ea7e27bf8be45460d739b78257bdb3
EsneiderMajin <emajin@unicauca.edu.co>
2025-02-05 22:33:43 -0500

    Estructuracion de Front con angular material.

--- Front/front-Observatorio/src/app/app.component.ts
+++ Front/front-Observatorio/src/app/app.component.ts
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'front-Observatorio';
+  nombre: string = 'Observatorio de Software';
 }
--- Front/front-Observatorio/src/app/app.module.ts
+++ Front/front-Observatorio/src/app/app.module.ts
@@ -3,14 +3,53 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './pages/header/header.component';
+import { FooterComponent } from './pages/footer/footer.component';
+import { RouterModule, Routes } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { HomeComponent } from './pages/Home/home.component';
+import { MatCardModule } from '@angular/material/card';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatGridListModule } from '@angular/material/grid-list';
+
+const routes: Routes = [
+  {
+    path:'',redirectTo:'/home',pathMatch:'full'
+  },
+  {
+    path:'home', component: HomeComponent
+  }
+  // {path:'clientes',component: ClientesComponent},
+  // {path:'cliente/form',component: FormComponent},
+  // {path:'cliente/form/:id',component: FormComponent}
+
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
+    MatCardModule,
+    MatExpansionModule,
+    MatGridListModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    RouterModule.forRoot(routes),
+    AppRoutingModule,
+    FormsModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]

1140a04ccb2b0659a9f40fc6a175c9b2f3b64771
EsneiderMajin <emajin@unicauca.edu.co>
2025-03-06 15:37:41 -0500

    agregacion de import.sql

---
+++ Back/back-Observatorio/src/main/resources/import.sql
@@ -0,0 +1,2 @@
+INSERT INTO `tipopregunta` (`idTipoPregunta`, `descripcion`, `nombre`) VALUES (1, 'Pregunta de selección única', 'Selección única'), (2, 'Pregunta de selección múltiple', 'Selección múltiple'), (3, 'Pregunta de respuesta corta', 'Respuesta corta'), (4, 'Pregunta de respuesta larga', 'Respuesta larga');
+

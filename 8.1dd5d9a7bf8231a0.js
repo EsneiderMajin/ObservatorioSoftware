c67c51b785dcf264973e858e6c4cc10beeb05eb3
EsneiderMajin <emajin@unicauca.edu.co>
2025-03-30 22:50:42 -0500

    despliegue front vista unicauca

---
+++ 8.1dd5d9a7bf8231a0.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkfront_Observatorio=self.webpackChunkfront_Observatorio||[]).push([[8],{8:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
